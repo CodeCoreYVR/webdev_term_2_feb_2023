e008bca07325a51bb9edf36231e93e8832a0d4e4
Ubuntu <nimbus-user@irdsn.codecore.usenimbus.com>
2023-03-02 02:01:14 +0000

    PostgreSQL with Knex: Migrating Tables

---
+++ postgres_with_knex/labs/knex_labs/knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'knex_labs_db',
+      user:     'nimbus-user',
+      password: 'password',
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'knex_labs_db',
+      user:     'nimbus-user',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'knex_labs_db',
+      user:     'nimbus-user',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
