d3080f23069424d005637980664e617da15ccdc4
Ubuntu <nimbus-user@irdsn.codecore.usenimbus.com>
2023-03-17 02:43:07 +0000

    JS Express Basics: Fizz Buzz

--- express_basics/labs/js_express_basics/app.js
+++ express_basics/labs/js_express_basics/app.js
@@ -55,6 +55,58 @@ app.post('/car_status', (request, response) => {
         carStatus
     });
 })
+// Set up a GET route for /fizz_buzz that renders a view called fizz_buzz.ejs
+app.get('/fizz_buzz', (request, response) => {
+  // Render the fizz_buzz view, passing in initial values for numbers, number1, and number2
+  // These values are set to null for now because the user hasn't entered anything yet
+  response.render('fizz_buzz', {
+    numbers: null,
+    number1: null,
+    number2: null
+  })
+})
+
+// Set up a POST route for /fizz_buzz that gets called when the user submits the form on the fizz_buzz view
+app.post('/fizz_buzz', (request, response) => {
+  // Parse the values for number1 and number2 from the request body
+  const number1 = parseInt(request.body.number1);
+  const number2 = parseInt(request.body.number2);
+
+  // Create an empty array to hold the FizzBuzz output
+  const numbers = [];
+
+  // Set up a counter variable to keep track of the current number
+  let counter = 1;
+
+  // Loop from 1 to 100, adding each number or FizzBuzz output to the numbers array
+  while (counter <= 100) {
+    // If the current number is divisible by both number1 and number2, add 'FizzBuzz' to the array
+    if (counter % number1 === 0 && counter % number2 === 0) {
+      numbers.push('FizzBuzz');
+    }
+    // If the current number is only divisible by number1, add 'Fizz' to the array
+    else if (counter % number1 === 0) {
+      numbers.push('Fizz');
+    }
+    // If the current number is only divisible by number2, add 'Buzz' to the array
+    else if( counter % number2 === 0) {
+      numbers.push('Buzz');
+    }
+    // If the current number is not divisible by either number1 or number2, add the number to the array
+    else {
+      numbers.push(counter);
+    }
+    // Increment the counter
+    counter++;
+  }
+
+  // Render the fizz_buzz view again, passing in the updated values for numbers, number1, and number2
+  response.render('fizz_buzz', {
+    numbers,
+    number1,
+    number2
+  })
+})
 
 // define the port and domain that the server will listen on
 const PORT = 3000;
