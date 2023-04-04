2929ab04ce9e545f597c3a0b06491fdfa44d8df7
Taif Al Musabe <taif@codecore.ca>
2023-04-04 15:23:24 -0500

    shown user for each post

--- express_and_rest/lecture/routes/posts.js
+++ express_and_rest/lecture/routes/posts.js
@@ -10,11 +10,12 @@ const router = express.Router()
 
 // the below path automatically assumes that the url has the "/posts" prefixed to it
 router.get('/', (req, res) => {
-    knex('posts')
-        .select("title", "content", "id")
-        .orderBy("created_at", "desc")
+    knex
+        .select("title", "content", "view_count", "username as posted_by","posts.id as id")
+        .from("posts")
+        .leftJoin("users", "users.id", "=", "posts.user_id")
         .then(posts => {
-            res.render('posts/index', { posts: posts })
+            res.render('posts/index', { posts })
         })
 })
 
@@ -33,7 +34,8 @@ router.post('/', (request, response) => {
             title: title,
             content: content,
             image_url: image_url,
-            posted_by: request.cookies.username || "Anonymous"
+            posted_by: request.session.user.username || "Anonymous",
+            user_id: request.session.user.id
         })
         .then(data => {
             response.redirect("/posts");
@@ -52,19 +54,21 @@ router.post('/', (request, response) => {
 // posts/133 ==> {id: 133}
 // posts/:id ===> :param name is used to show the information of the request.params
 
-router.get('/:id', (request, response) => {
-    const { id } = request.params; // const id = request.params.id;
+router.get('/:id', async (request, response) => {
+    const postId  = request.params.id; 
 
-    knex("posts")
-        .where("id", id)
-        .then(post => {
-            console.log(post)
-            response.render('posts/details', ...post);
-        })
-        .catch(ex => {
-            console.error(ex);
-            response.send("<h1>Something went wrong</h1>")
-        });
+    const post = await knex("posts").where('id', postId).first()
+    if (post) {
+        const user = await knex('users').where('id', post.user_id).first()
+        if(user) {
+            post.posted_by = user.username
+        } else {
+            post.posted_by = 'Anonymously'
+        }
+        response.render('posts/details', post);
+    } else {
+        response.send("<h1>Something went wrong</h1>")
+    }
 
 })
 
