79423eeb18accf566e0d86eefd7fb06474cee7c2
Debasish Das <debasish.das64@yahoo.com>
2023-04-24 12:54:06 -0700

    rails crud

--- rails/lecture/awesome_answer/app/controllers/questions_controller.rb
+++ rails/lecture/awesome_answer/app/controllers/questions_controller.rb
@@ -6,7 +6,45 @@ class QuestionsController < ApplicationController
 
     def create
         question_params = params.require(:question).permit(:title, :body)
-        Question.create(question_params)
+        @question = Question.new question_params
+
+        if @question.save
+            redirect_to question_path(@question)
+        else
+            render :new # it will go to 'def new' method
+            return false
+        end
+        # Question.create(question_params)
         # render text: "Question created successfully"
+    end
+
+    def show
+        @question = Question.find(params[:id])
+    end
+
+    def index
+        @questions = Question.order("created_at desc")
+    end
+
+    def edit
+        @question = Question.find(params[:id])
+    end
+
+    def update
+        question_params = params.require(:question).permit(:title, :body)
+        @question = Question.find params[:id]
+        
+        if @question.update (question_params)
+            redirect_to question_path(@question)
+        else
+            render :edit # it will go to 'def edit' method
+            return false
+        end
+    end    
+
+    def destroy
+        question = Question.find params[:id]
+        question.destroy
+        redirect_to questions_path
     end
 end
--- rails/lecture/awesome_answer/config/routes.rb
+++ rails/lecture/awesome_answer/config/routes.rb
@@ -12,4 +12,9 @@ Rails.application.routes.draw do
   # Question routes
   get "/questions/new" => "questions#new", as: :new_question
   post "/questions" => "questions#create", as: :questions #questions_path (_path is postfix)
+  get "/questions/:id" => "questions#show", as: :question #question_path (_path is postfix)
+  get "/questions" => "questions#index"
+  get "/questions/:id/edit" => "questions#edit", as: :edit_question
+  patch "/questions/:id" => "questions#update"
+  delete "/questions/:id" => "questions#destroy", as: :delete_question
 end
