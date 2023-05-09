c44db3d11bc0efa447e58f5d3b1a916732a990da
tam-codecore <taif@codecore.ca>
2023-05-09 12:08:44 -0500

    rails many to many: decomposing into two one to many associations

--- rails/lecture/awesome_answer/app/models/question.rb
+++ rails/lecture/awesome_answer/app/models/question.rb
@@ -3,13 +3,19 @@ class Question < ApplicationRecord
     has_many :answers, dependent: :destroy
     belongs_to :user, optional: true
 
-    has_and_belongs_to_many(
-        :likes, # this is the name/alias we want to give it
-        class_name: "User", # this is the name of the model we are associating with
-        join_table: "likes", # this is the name of join table
-        association_foreign_key: 'user_id', # this is the name of the key that will act as the foreign key
-        foreign_key: 'question_id' # this is the name of the key that will be used as the foreign key in the join tabile of this table
-    )
+    # To create join table associations without Like model:
+    # has_and_belongs_to_many(
+    #     :likes, # this is the name/alias we want to give it
+    #     class_name: "User", # this is the name of the model we are associating with
+    #     join_table: "likes", # this is the name of join table
+    #     association_foreign_key: 'user_id', # this is the name of the key that will act as the foreign key
+    #     foreign_key: 'question_id' # this is the name of the key that will be used as the foreign key in the join tabile of this table
+    # )
+
+    # To create join table associations Like model through decomposing:
+    has_many :likes, dependent: :destroy
+    # The user liking the question:
+    has_many :likers, through: :likes, source: :user
 
     validates :title, presence: true #, uniqueness: {message: "needs to be unique!"}
     validates :body, length: {minimum: 5} 
--- rails/lecture/awesome_answer/app/models/user.rb
+++ rails/lecture/awesome_answer/app/models/user.rb
@@ -4,13 +4,19 @@ class User < ApplicationRecord
     has_many :questions
     has_many :answers
 
-    has_and_belongs_to_many(
-        :liked_questions, # this is the name/alias we want to give it
-        class_name: "Question", # this is the name of the model we are associating with
-        join_table: "likes", # this is the name of join table
-        association_foreign_key: 'question_id', # this is the name of the key that will act as the foreign key
-        foreign_key: 'user_id' # this is the name of the key that will be used as the foreign key in the join tabile of this table
-    )
+    # To create join table associations without Like model:
+    # has_and_belongs_to_many(
+    #     :liked_questions, # this is the name/alias we want to give it
+    #     class_name: "Question", # this is the name of the model we are associating with
+    #     join_table: "likes", # this is the name of join table
+    #     association_foreign_key: 'question_id', # this is the name of the key that will act as the foreign key
+    #     foreign_key: 'user_id' # this is the name of the key that will be used as the foreign key in the join tabile of this table
+    # )
+
+    # To create join table associations Like model through decomposing:
+    has_many :likes, dependent: :destroy
+    # The user liking the question:
+    has_many :liked_questions, through: :likes, source: :question
 
     validates :email, presence: true, uniqueness: {message: "needs to be unique!"}
     validates :first_name, presence: true
